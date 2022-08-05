b968498c2da7fbc9449c92376f90938bb35a1004
happyjamy <juwhan1004@naver.com>
2022-08-06 03:30:01 +0900

    불필요한 app controller, app service 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,4 @@
-import { JwtService } from '@nestjs/jwt';
-import { UsersService } from './users/users.service';
-import { AuthService } from './auth/auth.service';
-import { UsersController } from './users/users.controller';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
@@ -18,7 +11,5 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
   ],
-  controllers: [AppController, AuthController, UsersController],
-  providers: [AppService, AuthService, UsersService, JwtService],
 })
 export class AppModule {}
