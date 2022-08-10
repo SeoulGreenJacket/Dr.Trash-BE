5682ee5f1beb2c90b9bb4aa424f7a9d22b8f0e4e
happyjamy <juwhan1004@naver.com>
2022-08-10 20:42:30 +0900

    user create할때 user,oauth table 분리 저장

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,10 @@ import { forwardRef, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { AuthModule } from 'src/auth/auth.module';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [forwardRef(() => AuthModule)],
+  imports: [forwardRef(() => AuthModule), DatabaseModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
