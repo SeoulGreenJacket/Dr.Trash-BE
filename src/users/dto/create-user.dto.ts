f11ce4818ea14739a1f85dab3ef2966571250cdf
happyjamy <juwhan1004@naver.com>
2022-08-10 20:44:18 +0900

    유저 생성시 필요한 type 추가

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,21 @@
 export class CreateUserDto {
+  point: number;
   name: string;
   thumnail: string;
-  email: string;
+}
+
+export class CreateOauthDto {
+  user_id: string;
+  provider: string;
+  id: string;
+}
+
+export class UserPayload {
+  name: string;
+  thumnail: string;
+}
+
+export class OauthPayload {
+  provider: string;
+  id: string;
 }
