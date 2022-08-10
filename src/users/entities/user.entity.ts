02818477df445913cd6a1ee50a70eadc3acabe58
happyjamy <juwhan1004@naver.com>
2022-08-10 20:52:12 +0900

    db 반영해서 User 스키마 변경

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -2,10 +2,5 @@ export class User {
   id: string;
   name: string;
   thumnail: string;
-  email: string;
-  //   point: number;
-  //   //grade: enum;
-  //   rank: number;
-  //   achievements: [];
-  //   //trashcan: []<>;
+  point: number;
 }
