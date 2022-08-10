c7fd4284044dfe14e84caa9cca169877a3f226cb
happyjamy <juwhan1004@naver.com>
2022-08-10 20:57:39 +0900

    JwtStrategy db 반영, RefreshJwtStrategy 생성

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,4 +1,4 @@
-import { UsersService } from './../../users/users.service';
+import { DatabaseService } from './../../database/database.service';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ExtractJwt, Strategy } from 'passport-jwt';
@@ -6,27 +6,45 @@ import { JwtPayload } from '../dto/jwt-payload.dto';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private usersServies: UsersService) {
-    const extractJwtFromCookie = (req) => {
-      let token = null;
-      if (req && req.cookies) {
-        token = req.cookies['jwt'];
-      }
-      return token || ExtractJwt.fromAuthHeaderAsBearerToken();
-    };
-
+  constructor(private databaseService: DatabaseService) {
     super({
-      jwtFromRequest: extractJwtFromCookie,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET,
     });
   }
 
   async validate(payload: JwtPayload) {
-    const user = this.usersServies.users.find((item) => item.id == payload.sub);
+    const user = this.databaseService.userFindByUser_id(payload.sub);
     if (!user) {
       throw new UnauthorizedException();
     }
+    return user;
+  }
+}
+
+@Injectable()
+export class RefreshJwtStrategy extends PassportStrategy(
+  Strategy,
+  'refresh-jwt',
+) {
+  constructor(private databaseService: DatabaseService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromBodyField('refresh_token'),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    let user: any = await this.databaseService.userFindByUser_id(payload.sub);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    user = {
+      ...user,
+      uuid: payload.uuid,
+    };
     return user;
   }
 }
