1a976c8c792b94576746663eb768447b807bca3b
happyjamy <juwhan1004@naver.com>
2022-08-10 20:56:19 +0900

    validateUser db 반영, logout 로직 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,12 @@
+import { UserPayload, OauthPayload } from './../users/dto/create-user.dto';
+import { DatabaseService } from './../database/database.service';
 import { UsersService } from 'src/users/users.service';
 import { Injectable } from '@nestjs/common';
 import { v4 as uuid4 } from 'uuid';
 import { JwtService } from '@nestjs/jwt';
 import { AuthRepository } from './auth.repository';
 import { User } from 'src/users/entities/user.entity';
+import { UserSosialDto } from './dto/user-sosial.dto';
 
 @Injectable()
 export class AuthService {
@@ -11,30 +14,39 @@ export class AuthService {
     private jwtService: JwtService,
     private authRepository: AuthRepository,
     private usersService: UsersService,
+    private databaseService: DatabaseService,
   ) {}
 
-  login(user: User) {
+  async login(user: User) {
     const uuid = uuid4();
     const payload = { uuid, sub: user.id };
     const access_token = this.jwtService.sign(payload);
     const expiresIn = parseInt(process.env.JWT_REFRESH_EXPIRES_IN);
     const refresh_token = this.jwtService.sign(payload, { expiresIn });
-    //this.authRepository.save(uuid, refresh_token, expiresIn);
+    this.authRepository.save(uuid, refresh_token, expiresIn);
     return {
       access_token,
       refresh_token,
     };
   }
 
-  validateUser(payload: any) {
-    const existedUser: User = this.usersService.users.find(
-      (item) => item.email == payload.email,
+  async validateUser(payload: UserSosialDto) {
+    const { id, provider, thumnail, name } = payload;
+    const existedUser: User = await this.databaseService.userFindByOauth(
+      id,
+      provider,
     );
     if (existedUser) {
       return this.login(existedUser);
     } else {
-      const user = this.usersService.create(payload);
+      const userPayload: UserPayload = { name, thumnail };
+      const oauthPayload: OauthPayload = { id, provider };
+      const user = await this.usersService.create(userPayload, oauthPayload);
       return this.login(user);
     }
+  }
+
+  async logout(user: any): Promise<void> {
+    await this.authRepository.delete(user.uuid);
   }
 }
