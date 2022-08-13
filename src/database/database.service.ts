f42060d7dcff3fcd2717805dc8e19d49ae94bbde
happyjamy <juwhan1004@naver.com>
2022-08-13 20:37:48 +0900

    database schema 환경변수 설정, 오류수정

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -4,33 +4,43 @@ import { NEST_PGPROMISE_CONNECTION } from 'nest-pgpromise';
 
 @Injectable()
 export class DatabaseService {
-  constructor(@Inject(NEST_PGPROMISE_CONNECTION) private readonly pg) {}
-
-  async query(sql: string) {
-    return await this.pg.query(sql);
+  appSchema: string;
+  constructor(@Inject(NEST_PGPROMISE_CONNECTION) private readonly pg) {
+    this.appSchema = process.env.DATABASE_APP_SCHEMA;
   }
 
   async userCreate(user: CreateUserDto) {
     const { name, image_uri, point } = user;
     return await this.pg.query(
-      `INSERT INTO application.user_table (name,image_uri,point) VALUES ('${name}', '${image_uri}', ${point}) RETURNING id;`,
+      `INSERT INTO ${this.appSchema}.user_table (name,image_uri,point) VALUES ('${name}', '${image_uri}', ${point}) RETURNING id;`,
     );
   }
 
   async oauthCreate(oauth: CreateOAuthDto) {
-    const { id, provider, user_id } = oauth;
+    const { oauth_id, provider, user_id } = oauth;
     return await this.pg.query(
-      `INSERT INTO oauth(user_id, provider, id) VALUES (${user_id}, ${id}, ${provider});`,
+      `INSERT INTO ${this.appSchema}.oauth(user_id, provider, oauth_id) VALUES (${user_id}, '${provider}', '${oauth_id}');`,
     );
   }
 
-  async userFindByOAuth(id: string, provider: string) {
-    return await this.pg.query(
-      `SELECT * FROM user WHERE id=(SELECT user_id FROM oauth WHERE id=${id} AND provider=${provider});`,
-    );
+  async userFindByOAuth(oauth_id: string, provider: string) {
+    const { user_id } = (
+      await this.pg.query(
+        `SELECT user_id FROM ${this.appSchema}.oauth WHERE oauth_id='${oauth_id}' AND provider='${provider}';`,
+      )
+    )[0];
+    return (
+      await this.pg.query(
+        `SELECT * FROM ${this.appSchema}.user_table WHERE id=${user_id};`,
+      )
+    )[0];
   }
 
-  async userFindByUser_id(user_id: string) {
-    return await this.pg.query(`SELECT * FROM user WHERE id=${user_id};`);
+  async userFindByUser_id(user_id: number) {
+    return (
+      await this.pg.query(
+        `SELECT * FROM ${this.appSchema}.user_table WHERE id=${user_id};`,
+      )
+    )[0];
   }
 }
