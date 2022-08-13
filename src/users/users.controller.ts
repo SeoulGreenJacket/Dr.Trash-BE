cfa1724210805e21d5cc67a3404977afe4e3ba44
happyjamy <juwhan1004@naver.com>
2022-08-13 18:04:50 +0900

    테스트 코드 삭제

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { DatabaseService } from 'src/database/database.service';
-import { CreateUserDto } from './dto/create-user.dto';
 
 @ApiTags('User')
 @Controller('users')
-export class UsersController {
-  constructor(private readonly databaseService: DatabaseService) {}
-
-  // 디비에 유저 생성 swagger 테스트용 코드
-  // @Post()
-  // async create(@Body() userCreateDto: CreateUserDto) {
-  //   return await this.databaseService.userCreate(userCreateDto);
-  // }
-}
+export class UsersController {}
