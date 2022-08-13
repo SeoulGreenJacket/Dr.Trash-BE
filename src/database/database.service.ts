441cf1c3747df04eb46a583d6805af9e7dd8d306
happyjamy <juwhan1004@naver.com>
2022-08-13 22:23:07 +0900

    DATABASE_APPLICATION_SCHEMA 환경변수 변경

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -9,16 +9,16 @@ import { NEST_PGPROMISE_CONNECTION } from 'nest-pgpromise';
 
 @Injectable()
 export class DatabaseService {
-  appSchema: string;
+  schema: string;
   constructor(@Inject(NEST_PGPROMISE_CONNECTION) private readonly pg) {
-    this.appSchema = process.env.DATABASE_APP_SCHEMA;
+    this.schema = process.env.DATABASE_APPLICATION_SCHEMA;
   }
 
   async userCreate(user: CreateUserDto) {
     const { name, image_uri, point } = user;
     return (
       await this.pg.query(
-        `INSERT INTO ${this.appSchema}.user_table (name,image_uri,point) VALUES ('${name}', '${image_uri}', ${point}) RETURNING id;`,
+        `INSERT INTO ${this.schema}.user_table (name,image_uri,point) VALUES ('${name}', '${image_uri}', ${point}) RETURNING id;`,
       )
     )[0].id;
   }
@@ -26,14 +26,14 @@ export class DatabaseService {
   async oauthCreate(oauth: CreateOAuthDto) {
     const { oauth_id, provider, user_id } = oauth;
     await this.pg.query(
-      `INSERT INTO ${this.appSchema}.oauth(user_id, provider, oauth_id) VALUES (${user_id}, '${provider}', '${oauth_id}');`,
+      `INSERT INTO ${this.schema}.oauth(user_id, provider, oauth_id) VALUES (${user_id}, '${provider}', '${oauth_id}');`,
     );
   }
 
   async userCheckByOAuth(oauth_id: string, provider: string): Promise<UserId> {
     return (
       await this.pg.query(
-        `SELECT user_id FROM ${this.appSchema}.oauth WHERE oauth_id='${oauth_id}' AND provider='${provider}';`,
+        `SELECT user_id FROM ${this.schema}.oauth WHERE oauth_id='${oauth_id}' AND provider='${provider}';`,
       )
     )[0];
   }
@@ -41,7 +41,7 @@ export class DatabaseService {
   async userFindByUser_id(user_id: number) {
     const user: User = (
       await this.pg.query(
-        `SELECT * FROM ${this.appSchema}.user_table WHERE id=${user_id};`,
+        `SELECT * FROM ${this.schema}.user_table WHERE id=${user_id};`,
       )
     )[0];
 
