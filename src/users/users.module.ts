8d4debc104426a4e6c54b479f8887ef0553a4be2
happyjamy <juwhan1004@naver.com>
2022-08-15 15:42:22 +0900

    UsersRepository 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,12 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { AuthModule } from './../auth/auth.module';
 import { DatabaseModule } from './../database/database.module';
+import { UsersRepository } from './users.repository';
 
 @Module({
   imports: [forwardRef(() => AuthModule), DatabaseModule],
   controllers: [UsersController],
-  providers: [UsersService],
-  exports: [UsersService],
+  providers: [UsersService, UsersRepository],
+  exports: [UsersService, UsersRepository],
 })
 export class UsersModule {}
