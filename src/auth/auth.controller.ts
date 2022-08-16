ef29b505257531c04f1f0b87760a09eeb91ea4b7
happyjamy <juwhan1004@naver.com>
2022-08-16 23:15:32 +0900

    유저 존재여부 검사를 kakao 전략에서 행하고 서비스에서는 로그인만 진행

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,7 +12,7 @@ export class AuthController {
   @UseGuards(KakaoAuthGuard)
   @Get('login/kakao')
   async loginWithKakao(@Req() req): Promise<JwtToken> {
-    return await this.authService.validateUser(req.user);
+    return await this.authService.login(req.user);
   }
 
   @UseGuards(RefreshJwtStrategy)
