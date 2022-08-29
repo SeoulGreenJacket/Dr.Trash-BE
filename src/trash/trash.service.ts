491cb3aafb2a8f02d4ecfafe87523ef603f61057
Seheon Yu <seheon@seoultech.ac.kr>
2022-08-29 22:50:31 +0900

    NEED REVERT

--- src/trash/trash.service.ts
+++ src/trash/trash.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { CacheService } from 'src/cache/cache.service';
+import { TrashSummary } from './dto/trash-summary.dto';
 import { TrashRepository } from './trash.repository';
 
 @Injectable()
 export class TrashService {
-  constructor(private readonly trashRepository: TrashRepository) {}
+  constructor(
+    private readonly trashRepository: TrashRepository,
+    private readonly cacheService: CacheService,
+  ) {}
 
   async beginTrashcanUsage(userId: number, trashcanId: number): Promise<void> {
     await this.trashRepository.logTrashcanUsage(userId, trashcanId, true);
@@ -11,5 +16,9 @@ export class TrashService {
 
   async endTrashcanUsage(userId: number, trashcanId: number): Promise<void> {
     await this.trashRepository.logTrashcanUsage(userId, trashcanId, false);
+  }
+
+  async getUserTrashSummary(userId: number): Promise<TrashSummary> {
+    return await this.cacheService.getUserTrashSummary(userId);
   }
 }
