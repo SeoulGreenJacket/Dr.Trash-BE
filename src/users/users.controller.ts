612d58c1fa4be930c2e79214072b3cf76521108d
happyjamy <juwhan1004@naver.com>
2022-08-31 14:18:51 +0900

    users 도메인 완성

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,62 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { CheckUserIdGuard } from './guard/check-user-id.guard';
+import { UserUpdateDto } from './dto/user-update.dto';
+import { UsersService } from 'src/users/users.service';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
 
 @ApiTags('User')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @Get('rank')
+  async findRankAll(
+    @Query('limit', ParseIntPipe, new DefaultValuePipe(10)) limit: number,
+    @Query('offset', ParseIntPipe, new DefaultValuePipe(0)) offset: number,
+  ): Promise<any> {
+    return await this.usersService.findRankAll(limit, offset);
+  }
+
+  @ApiBearerAuth()
+  @UseGuards(CheckUserIdGuard)
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  async findOne(@Param('id') id: number): Promise<any> {
+    return await this.usersService.findOne(id);
+  }
+
+  @ApiBearerAuth()
+  @UseGuards(CheckUserIdGuard)
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() userUpdateDto: UserUpdateDto,
+    @Req() req,
+  ): Promise<any> {
+    return await this.usersService.update(userUpdateDto, req.user);
+  }
+
+  @ApiBearerAuth()
+  @UseGuards(CheckUserIdGuard)
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id: number): Promise<any> {
+    return await this.usersService.delete(id);
+  }
+}
--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,31 +1,64 @@
 import { User } from 'src/users/entities/user.entity';
 import { Injectable } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
+import { Achievement } from './types/achievement.type';
 
 @Injectable()
 export class UsersRepository {
   constructor(private databaseService: DatabaseService) {}
   private readonly schema = process.env.DATABASE_APPLICATION_SCHEMA;
   private readonly userTable = `${this.schema}.user`;
+  private readonly achievementTable = `${this.schema}.achievement`;
+  private readonly achieverTable = `${this.schema}.achiever`;
 
-  async create(name: string, thumbnail: string, kakaoId: bigint) {
+  async create(
+    name: string,
+    thumbnail: string,
+    kakaoId: bigint,
+  ): Promise<number> {
     const result = await this.databaseService.query<{ id: number }>(
       `INSERT INTO ${this.userTable} ("kakaoId",name,thumbnail,point) VALUES (${kakaoId},'${name}', '${thumbnail}', 0) RETURNING id;`,
     );
     return result.length === 1 ? result[0].id : null;
   }
 
-  async findByKakaoId(kakaoId: bigint) {
-    const result = await this.databaseService.query<User>(`
-      SELECT * FROM ${this.userTable} WHERE "kakaoId" = ${kakaoId};
-    `);
+  async findByKakaoId(kakaoId: bigint): Promise<User> {
+    const result = await this.databaseService.query<User>(
+      `SELECT * FROM ${this.userTable} WHERE "kakaoId" = ${kakaoId};`,
+    );
     return result.length === 1 ? result[0] : null;
   }
 
-  async findByUserId(userId: number) {
+  async findByUserId(userId: number): Promise<User> {
     const result = await this.databaseService.query<User>(
       `SELECT * FROM ${this.userTable} WHERE id=${userId};`,
     );
     return result.length === 1 ? result[0] : null;
+  }
+
+  async findAcheiveByUserId(userId: number): Promise<Achievement[]> {
+    const result = await this.databaseService.query<Achievement>(
+      `SELECT ${this.achievementTable}.*,${this.achieverTable}."achievedAt"
+        FROM ${this.achievementTable} LEFT JOIN ${this.achieverTable}
+            ON ${this.achievementTable}.id = ${this.achieverTable}."achievementId"
+            AND ${this.achieverTable}."userId" = ${userId};`,
+    );
+    return result;
+  }
+
+  async update(name: string, thumbnail: string, id: number): Promise<User> {
+    const result = await this.databaseService.query<User>(
+      `UPDATE ${this.userTable} SET name='${name}', thumbnail='${thumbnail}' 
+        WHERE id=${id} 
+        RETURNING *;`,
+    );
+    return result.length === 1 ? result[0] : null;
+  }
+
+  async delete(id: number): Promise<number> {
+    const result = await this.databaseService.query<User>(
+      `DELETE FROM ${this.userTable} WHERE id=${id} RETURNING id;`,
+    );
+    return result.length === 1 ? result[0].id : null;
   }
 }
