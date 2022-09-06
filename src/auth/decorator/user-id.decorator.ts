fc28f3d18af13b25212b78532844cde029fef577
happyjamy <juwhan1004@naver.com>
2022-09-06 17:37:03 +0900

    UserId decorator User로 수정

--- src/auth/decorator/user-id.decorator.ts
+++ src/auth/decorator/user-id.decorator.ts
@@ -1,14 +1,10 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const UserId = createParamDecorator(
+export const User = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
     if (context.getType() === 'http') {
       const user = context.switchToHttp().getRequest().user;
-      if (user) {
-        return user.id;
-      } else {
-        return undefined;
-      }
+      return user;
     } else {
       throw new Error('User decorator only works with http requests');
     }
