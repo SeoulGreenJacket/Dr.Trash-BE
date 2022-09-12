efd9b600415cbeb2d729239ecbae736fd1d49970
happyjamy <juwhan1004@naver.com>
2022-09-12 17:25:37 +0900

    test

--- src/cache/cache.service.ts
+++ src/cache/cache.service.ts
@@ -177,6 +177,7 @@ export class CacheService {
           WHERE "userId" = ${userId} 
           AND "at" BETWEEN '${openString}' AND '${closeString}';`,
     );
+    console.log(trashLogs);
     let type: string,
       success = 0,
       failure = 0;
@@ -185,7 +186,9 @@ export class CacheService {
         const key = `user-trash:${userId}-${openString}`;
         const field = `${trashType}-${ok ? 'success' : 'failure'}`;
         type = trashType;
+        console.log(type);
         ok ? success++ : failure++;
+        console.log(success, failure);
         return this.client.hIncrBy(key, field, 1);
       }),
     );
