22c9b970a00a9529892ed0db4c5c80b280627da3
happyjamy <juwhan1004@naver.com>
2022-10-22 22:46:44 +0900

    articles domain crud complete

---
+++ src/articles/articles.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { ArticlesRepository } from './articlesRepository';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+
+@Injectable()
+export class ArticlesService {
+  constructor(private readonly articlesRepository: ArticlesRepository) {}
+
+  async create(createArticleDto: CreateArticleDto, authorId: number) {
+    return await this.articlesRepository.create(createArticleDto, authorId);
+  }
+
+  async findOne(id: number) {
+    await this.articlesRepository.increaseViewCount(id);
+    return await this.articlesRepository.findOne(id);
+  }
+
+  async findAll(orderBy, limit) {
+    return await this.articlesRepository.findAll(orderBy, limit);
+  }
+
+  async update(id: number, updateArticleDto: UpdateArticleDto) {
+    return await this.articlesRepository.update(id, updateArticleDto);
+  }
+
+  async remove(id: number) {
+    return await this.articlesRepository.remove(id);
+  }
+}
---
+++ src/articles/articlesRepository.ts
@@ -0,0 +1,64 @@
+import { CreateArticleDto } from './dto/create-article.dto';
+import { Injectable } from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
+import { Article } from './entities/article.entity';
+import { database } from 'src/common/environments';
+import { Order } from './enum/order.enum';
+
+@Injectable()
+export class ArticlesRepository {
+  constructor(private databaseService: DatabaseService) {}
+
+  async create(createArticleDto: CreateArticleDto, authorId: number) {
+    const { title, content } = createArticleDto;
+    const result = await this.databaseService.query<{ id: number }>(
+      `INSERT INTO ${database.tables.article} ("authorId",title,content,"viewCount") VALUES (${authorId},'${title}', '${content}',0)
+        RETURNING id;`,
+    );
+
+    return result.length === 1 ? result[0].id : null;
+  }
+
+  async findAll(orderBy: Order, limit: number) {
+    const limitValue = orderBy === 'viewCount' ? limit : 'ALL';
+    const result = await this.databaseService.query<Article[]>(`
+      SELECT * FROM ${database.tables.article}
+      ORDER BY "${orderBy}" DESC
+      LIMIT ${limitValue};
+    `);
+    return result;
+  }
+
+  async increaseViewCount(id: number) {
+    const result = await this.databaseService.query<{ viewCount: number }>(
+      `UPDATE ${database.tables.article} SET "viewCount"="viewCount"+1
+        WHERE id=${id} 
+        RETURNING "viewCount";`,
+    );
+    return result.length === 1 ? result[0].viewCount : null;
+  }
+
+  async findOne(id: number) {
+    const result = await this.databaseService.query<Article>(`
+      SELECT * FROM ${database.tables.article} WHERE id=${id};
+    `);
+    return result.length === 1 ? result[0] : null;
+  }
+
+  async update(id: number, updateArticleDto) {
+    const { title, content } = updateArticleDto;
+    const result = await this.databaseService.query<Article>(
+      `UPDATE ${database.tables.article} SET title='${title}', content='${content}' , "updatedAt"=NOW()
+        WHERE id=${id} 
+        RETURNING *;`,
+    );
+    return result.length === 1 ? result[0] : null;
+  }
+
+  async remove(id: number) {
+    const result = await this.databaseService.query<Article>(
+      `DELETE FROM ${database.tables.article} WHERE id=${id} RETURNING id;`,
+    );
+    return result.length === 1 ? result[0].id : null;
+  }
+}
